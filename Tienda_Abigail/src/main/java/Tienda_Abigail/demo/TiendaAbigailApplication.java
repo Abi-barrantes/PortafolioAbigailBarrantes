65c35265550c8ca8b8169f8a652f5a17e0dcb401
Abigail Barrantes <barrantesabigail09@gmail.com>
2026-02-11 20:12:57 -0600

    semana4
    Con git ignore

---
+++ Tienda_Abigail/src/main/java/Tienda_Abigail/demo/TiendaAbigailApplication.java
@@ -0,0 +1,15 @@
+package Tienda_Abigail.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication // <--- CORREGIDO: Solo se usa la anotación base
+@ComponentScan (basePackages = {"Tienda_IgnacioB", "com.tienda"}) // cambios
+public class TiendaAbigailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAbigailApplication.class, args);
+	}
+
+}
---
+++ Tienda_Abigail/src/test/java/Tienda_Abigail/demo/TiendaAbigailApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_Abigail.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAbigailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
